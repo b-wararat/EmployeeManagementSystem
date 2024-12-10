d65a3d5e20dae3a2b69b4186b34db40dc0db7d79
b-wararat <wararat.jjk@gmail.com>
2024-12-10 22:49:53 +0700

    create userAccountRepo for manage token. create account, and login

---
+++ ServerLibrary/Helpers/JwtSection.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ServerLibrary.Helpers
+{
+    public class JwtSection
+    {
+        public string? Key { get; set; }
+        public string? Issure { get; set; }
+        public string? Audience { get; set; }
+    }
+}
